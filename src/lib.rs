445410f8591a255a6bde6efbda0e06e296181730
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-10-26 15:07:07 -0700

    Adding tests for checked_add, checked_sub

--- src/lib.rs
+++ src/lib.rs
@@ -197,4 +197,46 @@ mod tests {
         FakeClock::advance_time(DUR);
         assert_eq!(Duration::from_millis(DUR), clock.elapsed());
     }
+
+    #[test]
+    fn test_checked_add_some() {
+        FakeClock::set_time(0);
+
+        let inst = FakeClock::now();
+        let dur = Duration::from_millis(std::u64::MAX);
+        FakeClock::set_time(std::u64::MAX);
+
+        assert_eq!(Some(FakeClock::now()), inst.checked_add(dur));
+    }
+
+    #[test]
+    fn test_checked_add_none() {
+        FakeClock::set_time(1);
+
+        let inst = FakeClock::now();
+        let dur = Duration::from_millis(std::u64::MAX);
+
+        assert_eq!(None, inst.checked_add(dur));
+    }
+
+    #[test]
+    fn test_checked_sub_some() {
+        FakeClock::set_time(std::u64::MAX);
+
+        let inst = FakeClock::now();
+        let dur = Duration::from_millis(std::u64::MAX);
+        FakeClock::set_time(0);
+
+        assert_eq!(Some(FakeClock::now()), inst.checked_sub(dur));
+    }
+
+    #[test]
+    fn test_checked_sub_none() {
+        FakeClock::set_time(std::u64::MAX - 1);
+
+        let inst = FakeClock::now();
+        let dur = Duration::from_millis(std::u64::MAX);
+
+        assert_eq!(None, inst.checked_sub(dur));
+    }
 }
